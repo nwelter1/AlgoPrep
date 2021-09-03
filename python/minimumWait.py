790865a12d61f60d226d731628d0f088fb72e661
nwelter1 <nwelter1@gmail.com>
2021-09-02 22:27:48 -0500

    moved mwt to complete

---
+++ python/minimumWait.py
@@ -0,0 +1,12 @@
+# Input: [3,2,1,2,6]
+
+def minimumWaitingTime(queries):
+	# Maybe just sort these... longest goes last....
+	new = sorted(queries)
+	total = 0
+	placeholder = 0
+	for i in range(1, len(new)):
+		placeholder += new[i-1]
+		total += placeholder
+		
+	return total
